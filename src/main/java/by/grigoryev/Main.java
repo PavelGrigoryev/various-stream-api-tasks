5dee9226f05c6a81e3067a915652f4049ac58c0f
Grigoryev Pavel <Green-ono@yandex.ru>
2023-02-11 18:52:57 +0300

    add task13

--- src/main/java/by/grigoryev/Main.java
+++ src/main/java/by/grigoryev/Main.java
@@ -8,7 +8,11 @@ import by.grigoryev.model.Person;
 import by.grigoryev.util.Util;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -93,7 +97,33 @@ public class Main {
 
     private static void task13() throws IOException {
         List<House> houses = Util.getHouses();
-        //        Продолжить...
+
+        List<Person> firsts = houses.stream()
+                .filter(house -> house.getBuildingType().equals("Hospital"))
+                .flatMap(house -> house.getPersonList().stream())
+                .toList();
+
+        List<Person> seconds = houses.stream()
+                .filter(house -> !house.getBuildingType().equals("Hospital"))
+                .flatMap(house -> house.getPersonList().stream()
+                        .filter(person -> ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) < 18
+                                          || (ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) >= 58
+                                              && person.getGender().equals("Female"))
+                                          || (ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) >= 63
+                                              && person.getGender().equals("Male"))))
+                .toList();
+
+        List<Person> thirds = houses.stream()
+                .flatMap(house -> house.getPersonList().stream())
+                .toList();
+
+        List<Person> evacuation = Stream.of(firsts, seconds, thirds)
+                .flatMap(Collection::stream)
+                .distinct()
+                .limit(500)
+                .toList();
+
+        evacuation.forEach(System.out::println);
     }
 
     private static void task14() throws IOException {
